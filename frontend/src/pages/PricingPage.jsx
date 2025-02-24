ca665b43a0d8c61011242f0fa77609b4c1498c8d
samarpan sarkar <69873435+samarpansarkar@users.noreply.github.com>
2025-02-24 21:24:58 +0530

    Update PricingPage.jsx

--- frontend/src/pages/PricingPage.jsx
+++ frontend/src/pages/PricingPage.jsx
@@ -3,7 +3,7 @@ import { FaPersonSkiing } from "react-icons/fa6";
 import { SiEnterprisedb } from "react-icons/si";
 const PricingPage = () => {
   return (
-    <div className='relative w-screen h-screen bg-green-200'>
+    <div className='flex h-screen bg-green-200 rounded-4xl mb-10'>
       <div className='mx-auto px-4 py-16 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:py-20'>
         <div className='mb-10 max-w-xl sm:text-center md:mx-auto md:mb-12 lg:max-w-2xl'>
           <h2 className='mb-6 max-w-lg font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto'>
