43fa263abc43975993e42cec603008b9d7f794c7
samarpansarkar <samarpansarkar209@gmail.com>
2026-03-02 15:27:07 +0530

    ADD: user model

---
+++ next-bookmystay/src/models/User.model.js
@@ -0,0 +1,18 @@
+import { timeStamp } from "console";
+import mongoose from "mongoose";
+
+const { Schema, models, model } = mongoose;
+
+const userSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    passwordHash: { type: String, required: true },
+    role: { type: String, enum: ["customer", "admin", "owner"], default: "customer" },
+    createdAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true },
+);
+
+export default models.User || model("User", userSchema);
